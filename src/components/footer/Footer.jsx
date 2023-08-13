fe826e505a7116f8e9fccef932f42c2704f2da23
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-08-13 22:46:32 +0100

    Skills and exp fixes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -18,10 +18,10 @@ const Footer = () => {
           <a href="#about">About</a>
         </li>
         <li>
-          <a href="#experience">Experience</a>
+          <a href="#experience">Skills</a>
         </li>
         <li>
-          <a href="#services">Services</a>
+          <a href="#services">Experience</a>
         </li>
         <li>
           <a href="#portfolio">Portfolio</a>
